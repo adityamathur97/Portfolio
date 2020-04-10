5a5ba251cd45239c3dca49d738587a1826bd1acf
Aditya Mathur <adityamathur97@gmail.com>
2020-04-10 01:56:21 -0500

    About component - changed size, padding and font

--- src/components/About.js
+++ src/components/About.js
@@ -12,19 +12,19 @@ class About extends React.Component {
 		return (
 			<div className="card mb-3 aboutCard">
 				<div className="row no-gutters">
-					<div className="col-md-6">
+					<div className="col-md-7">
 						<div className="card-body">
-							<h5 className="card-title">Card title</h5>
-							<p className="card-text">
-								This is a wider card with supporting text below as a natural lead-in to additional
-								content. This content is a little bit longer.
+							<h1 className="card-title cardHeading">Hello Folks! I'M A Developer</h1>
+							<p className="card-text cardDesc">
+								I'm a graduate student studying Master's in Computer Science at University of Texas at
+								Dallas.
 							</p>
 							<p className="card-text">
 								<small className="text-muted">Last updated 3 mins ago</small>
 							</p>
 						</div>
 					</div>
-					<div className="col-md-6">
+					<div className="col-md-5">
 						<img
 							src={require('../images/software_engineers.svg')}
 							className="card-img aboutLogo"
