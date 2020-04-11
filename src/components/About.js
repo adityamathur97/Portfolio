e422199d9ffc7a6be4b69ffe8a5d091988813ad1
Aditya Mathur <adityamathur97@gmail.com>
2020-04-11 18:08:31 -0500

    added fading property to text

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,29 @@ import $ from 'jquery';
 class About extends React.Component {
 	constructor() {
 		super();
+		this.textTurn = false;
+	}
+
+	componentDidMount() {
+		$('#changeText').fadeOut(0, function() {
+			$('#changeText').fadeIn(3500, function() {
+				$('#changeText').fadeOut(3500);
+			});
+		});
+		setInterval(function() {
+			var text;
+			$('#changeText').fadeOut(3500, function() {
+				$('#changeText').empty();
+				if (this.textTurn == true) {
+					text = 'Aditya';
+				} else {
+					text = 'A Developer';
+				}
+				$('#changeText').append(text);
+				$('#changeText').fadeIn(3500);
+				this.textTurn = !this.textTurn;
+			});
+		}, 7000);
 	}
 
 	render() {
@@ -14,11 +37,16 @@ class About extends React.Component {
 				<div className="row no-gutters">
 					<div className="col-md-7">
 						<div className="card-body">
-							<h1 className="card-title cardHeading">Hello Folks! I'M A Developer</h1>
+							<h1 className="card-title cardHeading">
+								Hello Folks! I'M <span id="changeText">Aditya</span>
+							</h1>
 							<p className="card-text cardDesc">
 								I'm a graduate student studying Master's in Computer Science at University of Texas at
 								Dallas.
 							</p>
+							<button type="button" class="btn btn-primary btn-lg contButton">
+								Continue
+							</button>
 							<p className="card-text">
 								<small className="text-muted">Last updated 3 mins ago</small>
 							</p>
