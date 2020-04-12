9a36ec7a542b9b218442ab9f38dc3c73c41afc19
Aditya Mathur <adityamathur97@gmail.com>
2020-04-11 19:18:08 -0500

    added skyline image to bottom of page

---
+++ src/components/SkyLine.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../css/SkyLine.css';
+
+class SkyLine extends React.Component {
+	constructor() {
+		super();
+	}
+
+	render() {
+		return <div className="skyLineDiv" />;
+	}
+}
+
+export default SkyLine;
