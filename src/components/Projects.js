d3ac43cd5990661a0a7f4cb551a3630e4052e824
Aditya Mathur <adityamathur97@gmail.com>
2020-04-12 23:44:59 -0500

    added Projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ProjectData from '../data/project.json';
+
+import $ from 'jquery';
+import '../css/Projects.css';
+
+class Projects extends React.Component {
+	componentDidMount() {
+		$(ProjectData).each(function(key, value) {
+			var eachProject =
+				'<div class="projectDiv">' +
+				'<a class="projectTitleLink" target="_blank" href=' +
+				value.link +
+				'><h2 class="projectTitle">' +
+				value.heading +
+				'</h2></a>' +
+				'<p class="projectDate">' +
+				value.time +
+				'</p>' +
+				'<p class="projectDesc">' +
+				value.about +
+				'</p>' +
+				'</div>';
+			$('#projectContent').append(eachProject);
+		});
+	}
+
+	render() {
+		return (
+			<div className="projectsContainer shadow p-3 mb-5 bg-white rounded">
+				<i className="fas fa-project-diagram clipIcon" />
+				<h1 id="projectLink">Projects</h1>
+				<div id="projectContent" />
+			</div>
+		);
+	}
+}
+
+export default Projects;
